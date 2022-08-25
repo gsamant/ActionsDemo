39be9a015ae161399f3fd9191be2fca326a5cf05
Gandhali <gsamant@github.com>
2022-08-25 19:28:07 +0530

    fixed

--- StringLibrary/StringLibrary.cs
+++ StringLibrary/StringLibrary.cs
@@ -9,13 +9,8 @@ namespace UtilityLibraries
             if (string.IsNullOrWhiteSpace(str))
                 return false;
 
-<<<<<<< HEAD
-            char ch = str[0];
-            return char.IsLower(ch);
-=======
             char chr = str[0];
             return char.IsUpper(chr);
->>>>>>> cb34ed596f2f65f77411aa51f8d9ba592513d6d7
-        }
+       }
     }
 }
